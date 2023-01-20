1d9566c7a185556abf946bc316565da682e90a47
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-01-19 23:07:08 -0700

    Used a use effect to fetch the data

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 export const useFetch = (options) => {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    console.log(options)
+    fetch(options.url)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+    }, [])
+
 
   return {
     data,
   }
-
 }
