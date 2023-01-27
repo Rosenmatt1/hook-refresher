d3db5e7b5a743b0007455de9ab4139f5d2b88b06
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-01-27 04:39:02 -0700

    Tweaked the useStopWatch function to use conditionals instead of setInterval

--- src/App.js
+++ src/App.js
@@ -4,17 +4,36 @@ import { useFetch } from "./useFetch"
 import './App.css';
 
 const useStopWatch = () => {
-  const [count, setCount ] = useState(0)
+  const [count, setCount] = useState(0)
+
+  //   useEffect(() => {
+  //     const interval = setInterval(() => {
+  //       console.log(`count = ${count}`)
+  //       setCount((prev) => prev +1)
+  //       if (count >= 10)  return count
+
+  //     }, 1000)
+
+  //     return () => clearInterval(interval)
+  //   }, []) //useEffect would keep getting rerun if count was a dependency
+  //   return count
+  // }
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      console.log(`count = ${count}`)
-      setCount((prev) => prev +1)
-    }, 1000)
-    
-    return () => clearInterval(interval)
-  }, []) //useEffect would keep getting rerun if count was a dependency
+    if (count >= 10) {
+      return
+    }
+    else if (count < 10) {
+      setCount(count + 1)
+    }
 
+    console.log(count)
+    // const interval = setInterval(() => {
+    //   console.log(`count = ${count}`)
+    //   // setCount((prev) => prev + 1)
+
+    // }, 1000)
+  }, [count])
   return count
 }
 
@@ -22,7 +41,7 @@ const useStopWatch = () => {
 function App() {
   const [url, setUrl] = useState(null)
   const count = useStopWatch()
-  const { data } = useFetch({url})
+  const { data } = useFetch({ url })
 
 
   return (
